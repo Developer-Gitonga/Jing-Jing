af86faef4ac9d78ded7670f36d77d679b2c3dce6
Develper-Gitonga <aizensalim52@gmail.com>
2022-07-04 12:37:13 +0300

    ch: boilerplate update

--- public/settings.py
+++ public/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'src',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
