b189280086ddcc700b16d04bcb42de51a3d09c01
Develper-Gitonga <aizensalim52@gmail.com>
2022-07-05 15:39:55 +0300

    ch: boilerplate update

--- src/urls.py
+++ src/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import *
-from src import views
+from . import views
 
 urlpatterns = [
     path('', views.Home, name='home'),
     path('cart/', views.Cart, name='cart'),
     path('signup/', views.Signup, name='signup'),
     path('login/', views.Login, name='login'),
+    path('logout/', views.logout, name='logout'),
+
     # path('logout/', LogoutView.as_view(), name='logout'),
-    # path('profile/<int:pk>/update/', views.edit_user, name='edit_profile'),
+    # path('profile/<int:pk>/update/', views.edit_profile, name='edit_profile'),
     # path('profile/', views.Profile, name='profile'),
     # path('posts/',  views.Posted , name="posts" ),
     # path('posts/create',  views.create_post , name="create_posts" ),
--- src/views.py
+++ src/views.py
@@ -3,6 +3,9 @@ from django.views import View
 from .forms import SignupForm,SearchForm, LoginForm
 from urllib import request
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout
+
 
 
 
@@ -28,7 +31,12 @@ def Login(request):
                 return HttpResponse('Such a user does not exist')
         else:
             return HttpResponse("Form is not Valid")
-    return render(request,'jingle/login.html',{'form':form})
+    return render(request,'jingle/home.html',{'form':form})
+
+
+def logout(request):
+    user.logout(request)
+    return redirect('/')
 
 
 def Signup(request):
@@ -40,3 +48,57 @@ def Signup(request):
     else:
         form = SignupForm()
     return render(request,'jingle/sign-up.html', {"form":form})
+
+
+# # class ProfileView(LoginRequiredMixin, View):
+# #     login_url = '/login/'
+# #     """this class view is used to render the profile page and execute user profile updates."""
+
+#     def Profile(self, request):
+#         user = request.user
+        
+#         profile = get_object_or_404(UserProfile, user=user)
+#         context = {
+#             'title': 'Profile',
+#             'user_data': user,
+#             'profile_data': profile,
+           
+#         }
+#         return render(request, 'jingle/profile.html')
+
+
+# # profile design
+# # @login_required
+# def edit_profile(request, pk):
+#     user = User.objects.get(pk=pk)
+
+#     # prepolate the form with the user's data
+#     user_form = UserForm(instance=user)
+
+#     ProfileInlineFormSet = inlineformset_factory(
+#         User, UserProfile, fields=('bio', 'phone', 'picture'), extra=0)
+#     formset = ProfileInlineFormSet(instance=user)
+
+#     if request.user.is_authenticated and request.user.id == user.id:
+#         if request.method == 'POST':
+#             user_form = UserForm(
+#                 request.POST, request.FILES, instance=user)
+#             formset = ProfileInlineFormSet(
+#                 request.POST, request.FILES, instance=user)
+
+#             if user_form.is_valid():
+#                 created_user = user_form.save(commit=False)
+#                 formset = ProfileInlineFormSet(
+#                     request.POST, request.FILES, instance=created_user)
+#                 if formset.is_valid():
+#                     created_user.save()
+#                     formset.save()
+#                     return HttpResponseRedirect('/profile/')
+
+#         return render(request, 'jingke/edit_profile.html', {
+#             "pk": pk,
+#             "user_form": user_form,
+#             "formset": formset,
+#         })
+#     else:
+#         raise PermissionDenied
